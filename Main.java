e3f09d2f60154a022812ed90ac2b8f46ff22223c
Johnny Dreher <johnnydreher@gmail.com>
2026-07-15 17:21:38 +0000

    Adicionadas inicialização e funções de personagens

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        Personagem heroi2 = new Personagem("Aremis", "Arqueira", 5, 80, 12.5);
 
-        .
-        .
-        .
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
 
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -7,17 +7,20 @@ public class Personagem {
 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
 
     }
 
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+        System.out.println("Nome: "+this.nome);
+        System.out.println("Classe: "+this.classe);
+        System.out.println("Nivel: "+this.nivel);      
+        System.out.println("Pontos de Vida: "+this.pontosDeVida);
+        System.out.println("Pode Base: "+this.poderBase); 
 
     }
 }
